db77bd797441425d089ffd7beb15d63dd23d4330
David Felix <davidffelix@hotmail.com>
2025-08-14 06:46:59 -0300

    feat: add Big Sky files and model class

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,11 @@
+class WeatherModel {
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
+
+  WeatherModel({
+    required this.cityName,
+    required this.temperature,
+    required this.mainCondition,
+  });
+}
