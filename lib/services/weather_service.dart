f7db467480970bc8fef71ae64958d1994129c6e6
David Felix <davidffelix@hotmail.com>
2025-08-18 07:05:05 -0300

    feat: Integrate API endpoint into WeatherService

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,1 +1,9 @@
-class WeatherService {}
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const String BASE_URL =
+      'https://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+}
